eb37cd6ed2472d445c40abbf9325b7d759db30f8
lavendahkinyua <lavendah17@gmail.com>
2023-07-02 13:01:06 -0700

    task 6

--- console.py
+++ console.py
@@ -1,17 +1,42 @@
+"""
+This module contains the command interpreter for the HBNB program.
+
+The command interpreter provides a command line interface for interacting with the HBNB application.
+"""
+
 import cmd
+
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb)'
-    def do_quit(self, args):
-        """Quit command to exit the program"""
+    """
+    Command interpreter for the HBNB program.
+
+    This class extends the `cmd.Cmd` class and provides custom commands and prompt for the HBNB application.
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit the command interpreter.
+
+        Usage: quit
+        """
         return True
 
-    def do_EOF(self, args):
-        """Exit the program"""
+    def do_EOF(self, arg):
+        """
+        Exit the command interpreter.
+
+        This command can be used by pressing Ctrl+D or entering 'EOF'.
+        """
         return True
 
     def emptyline(self):
-        """Do nothing when an empty line is entered"""
+        """
+        Do nothing when an empty line is entered.
+        """
         pass
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
+
