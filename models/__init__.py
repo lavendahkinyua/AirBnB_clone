840d5b4801f550f127a88e43e4e5e0a1b7da39b2
lavendahKinyua <lavendah17@gmail.com>
2023-07-11 02:21:35 -0700

    commit changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,5 @@
-from models.engine.file_storage import file_storage.py
+#!/usr/bin/python3
+"""Initializes the package"""
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -6,36 +6,49 @@ import uuid
 from datetime import datetime
 #defining a class
 class BaseModel:
-#instance attribute ..self is an object placeholder that why we use use it to define instance attributes it will be replaced with actual objects when we are calling the object
-  def __init__(self, *args, **kwargs):
-    self.id = str(uuid.uuid4())
-    self.created_at = datetime.now()
-    self.updated_at = datetime.now()
-    
-    if kwargs:
-        for key, value in kwargs.items():
-            if key != '__class__':
-                if key in ['created_at', 'updated_at']:
-                    date_obj = datetime.strptime(value,'%Y-%m-%dT%H:%M:%S.%f')
-    else:
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+#instance attribute ..self is an object placeholder that why we use use it to define instance attributes it will be replaced with actual objects when we are calling the objec
+    def __init__(self, *args, **kwargs):
+        """Initializes instance attributes
 
-  
-  def __str__(self):
-     return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
-  def save(self):
-    self.updated_at = datetime.now()
-    storage.save()
-  def to_dict(self):
-    valuesdict = {'__class__':(type(self).__name__)}
-    for key,value in self.__dict__.items():
-      if isinstance(value,datetime):
-        #value = datetime.now()
-        value_str = value.isoformat()
-        valuesdict[key] = value_str
-      else:
-        valuesdict[key] = value
-    return valuesdict
+        Args:
+            - *args: list of arguments
+            - **kwargs: dict of key-values arguments
+        """
+
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """Returns official string representation"""
+
+        return "[{}] ({}) {}".\
+            format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__"""
+
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
 
