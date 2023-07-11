eb76aac249a9f2b13e50cc62c85395d934860a51
lavendahKinyua <lavendah17@gmail.com>
2023-07-11 02:45:11 -0700

    commit changes

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,11 @@
 """ defining a class basemodel """
 from models import storage
 import uuid
-#the reason we have used from its because the datetime class is in a module
+    #the reason we have used from its because the datetime class is in a module
 from datetime import datetime
 #defining a class
 class BaseModel:
-#instance attribute ..self is an object placeholder that why we use use it to define instance attributes it will be replaced with actual objects when we are calling the objec
+    #instance attribute
     def __init__(self, *args, **kwargs):
         """Initializes instance attributes
 
